e7e8c56568bf31e7b2750b9b30f3e39028af939f
ChrGri <christophergrimm88@web.de>
2025-09-23 16:09:13 +0000

    Deploying to gh-pages from @ ChrGri/FastNonAccelStepper@055f88217f5078e139fbf639a73d0a27d69bd831 🚀

--- html/search/defines_0.js
+++ html/search/defines_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['max_5fallowed_5fposition_5fchange_5fper_5fcycle_0',['MAX_ALLOWED_POSITION_CHANGE_PER_CYCLE',['../FastNonAccelStepper_8cpp.html#a6dc832eec948b3b53c77e5808d88134a',1,'FastNonAccelStepper.cpp']]],
-  ['max_5fspeed_5fin_5fhz_1',['MAX_SPEED_IN_HZ',['../FastNonAccelStepper_8h.html#a18e9ada63911ebf21d4c07047ee555b9',1,'FastNonAccelStepper.h']]],
+  ['max_5fspeed_5fin_5fhz_1',['MAX_SPEED_IN_HZ',['../FastNonAccelStepper_8cpp.html#a18e9ada63911ebf21d4c07047ee555b9',1,'MAX_SPEED_IN_HZ():&#160;FastNonAccelStepper.cpp'],['../FastNonAccelStepper_8h.html#a18e9ada63911ebf21d4c07047ee555b9',1,'MAX_SPEED_IN_HZ():&#160;FastNonAccelStepper.h']]],
   ['mcpwm_5fpcnt_5fmax_5fallowed_5fmovement_5fin_5fopposite_5fdir_5ftill_5fstop_2',['MCPWM_PCNT_MAX_ALLOWED_MOVEMENT_IN_OPPOSITE_DIR_TILL_STOP',['../FastNonAccelStepper_8cpp.html#a5b3df6c492b90b425304215f842c45a2',1,'FastNonAccelStepper.cpp']]]
 ];
